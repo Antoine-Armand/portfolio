d47385a80f2596a801e64422eec205f015f88358
Antoine-Armand <antoine.armand99@gmail.com>
2019-10-03 19:22:57 +0200

    Working on home page
    Adding container and content

--- index.php
+++ index.php
@@ -1,46 +1,8 @@
 <?php
     require_once 'views/partials/header.php';
-    include 'views/partials/menu.php';
-?>
-    <div class="container">
-        <div class="introduction">
-            <h1> Antoine Armand </h1>
-            <p> 
-                Etudiant et auto-entrepreneur en tant que webmaster. Je suis actuellement à la recherche d'un stage de 3 mois de juillet à octobre. 
-            </p>
-            <h4> Pour me contacter : antoine.armand99@gmail.com </h4>
-        </div>
-        <div class="projectContainer">
-            <h2> Liste de projets </h2>
 
-            <div class="projectList">
-                <div class="projects project_1">
-                    <h3> 🔍 Projet de montée en gamme de Timberland </h3>
-                    <p> 
-                        Réalisation d'un dossier de 84 pages présentant les stratégies actuelles et futures de Timberland pour mener à sa montée en gamme. Annexé d'une présentation du dossier sous forme théatrale.
-                    </p>
-                </div>
-                <div class="projects project_2">
-                    <h3> 🔧 Site Pokédex Kanto </h3>
-                    <p> 
-                        Réalisation d'un site web en utilisant la PokeAPI avec du PHP. Présente le pokédex de Kanto qui comprend 150 pokémons environ. Permet d'obtenir des caractéristiques sur les pokémons.
-                    </p>
-                </div>
-                <div class="projects project_2">
-                    <h3> 🎨 Refonte de l'application mobile Le Bonbon </h3>
-                    <p> 
-                        Réalisation de deux écrans selon l'UI d'Android. Le but est de simplifier l'utilisation tout en respectant les bons usages Android.
-                    </p>
-                </div>
-                <div class="projects project_2">
-                    <h3> 🔧 Application To Do List </h3>
-                    <p> 
-                        Designer et coder quatre écrans pour une application Android sur du Kotlin. 
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-<?php
+    include 'views/partials/menu.php';
+    include 'views/pages/home.php';
+
     require_once 'views/partials/footer.php';
 ?>
---
+++ views/pages/home.php
@@ -0,0 +1,10 @@
+<main class="main-container">
+    <div class="image-container">
+        <div class="image-contents">
+            <img class="images" src="#" alt='work_1'>
+        </div>
+        <div class="image-contents">
+            <img class="images" src="#" alt='work_1'>
+        </div>
+    </div>
+</main>
