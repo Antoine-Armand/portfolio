d5fd73379b84a85d8b2bbe9736c7d98d63690987
Antoine-Armand <antoine.armand99@gmail.com>
2019-10-04 18:03:02 +0200

    Adding titles
    Adding title for the page
    Adding title for the project
    Adding label for the project

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,10 +1,15 @@
 <main class="main-container">
+    <h1 class="main-title"> Antoine </h1>
     <div class="image-container">
-        <div class="image-contents image-top">
-            <img class="images" src="assets/img/image_replace_1.jpg" alt='work_1'>
+        <div class="image-contents image-content-top">
+            <img class="images image-top" src="assets/img/image_replace_1.jpg" alt='work_1'>
         </div>
-        <div class="image-contents image-bottom">
-            <img class="images" src="assets/img/image_replace_1.jpg" alt='work_1'>
+        <div class="image-contents image-content-bottom">
+            <img class="images image-bottom" src="assets/img/image_replace_1.jpg" alt='work_1'>
         </div>
+    </div>
+    <div class="project-text">
+        <h3 class="project-label"> Design UX/UI </h3>
+        <h2 class="project-title"> Airbnb Weekends </h2>
     </div>
 </main>
