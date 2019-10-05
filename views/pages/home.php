cb550beda950d8e914be91aad30082081e1d2a25
Antoine-Armand <antoine.armand99@gmail.com>
2019-10-05 19:17:57 +0200

    Adding menu choice
    Correct title
    Adding the menu choice

--- views/pages/home.php
+++ views/pages/home.php
@@ -7,9 +7,16 @@
         <div class="image-contents image-content-bottom">
             <img class="images image-bottom" src="assets/img/image_replace_1.jpg" alt='work_1'>
         </div>
+        <div class="project-text">
+            <h3 class="project-label"> Design UX/UI </h3>
+            <h2 class="project-title"> Airbnb Weekends </h2>
+        </div>
     </div>
-    <div class="project-text">
-        <h3 class="project-label"> Design UX/UI </h3>
-        <h2 class="project-title"> Airbnb Weekends </h2>
+    <div class="choice-menu">
+        <ul>
+            <li class="choice-menu-objects"> 1 </li>
+            <li class="choice-menu-objects"> 2 </li>
+            <li class="choice-menu-objects"> 3 </li>
+        </ul>
     </div>
 </main>
