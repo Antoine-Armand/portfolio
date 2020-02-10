74db6c77889f520f7c07198c99ae1391a310ceef
Antoine-Armand <antoine.armand99@gmail.com>
2020-02-10 21:53:03 +0100

    Project re-onboarding
    Try to correct the problem of the images's hover in the home page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,11 @@
-choiceMenuObjects = document.querySelectorAll('.choice-menu-objects')
+console.log("hello");
 
-
+const animation = bodymovin.loadAnimation({
+    container: document.getElementsByClassName('main-container'),
+    renderer: 'svg',
+    type: 'canvas',
+    loop: true,
+    autoplay: true,
+    prerender: true,
+    path: './img/animation-1.json'
+})
--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,2 +1,4 @@
+<script src="assets/scripts/lottie.js"></script>
+<script src="assets/scripts/main.js"></script>
 </body>
 </html>
